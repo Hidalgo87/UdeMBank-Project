9d902ca480bc3b54df96e01ff056b52d74feba54
Znorlux <christianlombardi0414@gmail.com>
2023-02-18 11:24:56 -0500

    commit2

--- ATM.java
+++ ATM.java
@@ -7,7 +7,7 @@ class ATM
     {
         this.id = id_ingresado;
         this.balance = balance_ingresado;
-        //Prueba repositorio
+        //Prueba repositorio por mi
     }
 
   //properties en c#
