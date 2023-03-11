bc8f82575f2f00f7c9ebf66a38d410c642767d8c
JUAN FERNANDO <juanfgomez6@gmail.com>
2023-03-10 19:19:22 -0500

    testing

--- Proyecto/MainClass.java
+++ Proyecto/MainClass.java
@@ -3,7 +3,7 @@ class MainClass
   public static void main(String[] args)
   {
     Bank bank = new Bank();
-    bank.Run(bank);
+    bank.Run();
 
 
     //ManejadorArchivo manejador = new ManejadorArchivo();
