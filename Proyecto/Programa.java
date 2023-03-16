3c2e1280fc2cd36cffb84d87cf477492b0c46fcd
JUAN FERNANDO <juanfgomez6@gmail.com>
2023-03-16 17:49:59 -0500

    Se borraron comentarios innecesarios.

--- Proyecto/Programa.java
+++ Proyecto/Programa.java
@@ -4,9 +4,5 @@ class Programa
   {
     Bank bank = new Bank();
     bank.Run();
-    
-    //ManejadorArchivo manejador = new ManejadorArchivo();
-    //manejador.verificar_password(0, "admin123");
-    //manejador.escribir_nuevo_usuario(4, "eaea", 400);
   }
 }
